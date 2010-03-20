d6e01473bd4b14380164b912e00e87033b5a5876
Kathryn Huxtable <kathryn@kathrynhuxtable.org>
2010-03-20 11:26:52 -0500

    Organized imports.

--- src/main/java/org/kathrynhuxtable/maven/plugins/htmlfiltersite/FilterMojo.java
+++ src/main/java/org/kathrynhuxtable/maven/plugins/htmlfiltersite/FilterMojo.java
@@ -30,9 +30,7 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
-
 import java.text.SimpleDateFormat;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -41,27 +39,20 @@ import java.util.List;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.exception.MethodInvocationException;
 import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
-
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.PathTool;
 import org.codehaus.plexus.util.StringUtils;
-
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
-
 import org.jdom.input.SAXBuilder;
-
 import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
-
 import org.jdom.xpath.XPath;
 
 /**
